4116e42648fb6048d24a41a0bac2561fd5fa8ed8
rkronberg <43936697+rkronberg@users.noreply.github.com>
2018-10-14 13:25:17 +0300

    Implemented calculation of atomization energy and reading of mu, alpha, charges from dataset

--- machine.py
+++ machine.py
@@ -21,17 +21,23 @@ def preprocess(datasize):
 	# Initialize the variables as empty lists
 	# natoms = number of atoms in a given molecule
 	# nonHatoms = number of non-H atoms in a given molecule
-	# u0 = internal energy at 0 K
+	# Ea = Atomization energy (Ha)
+	# mu = Dipole moment (Debye
+	# alpha = Isotropic polarizability (bohr^3)
 	# atomlist = list of the atoms constituting a given molecule (e.g. [C,H,H,H] for methane)
 	# coords = xyz coordinates of each atom in a given molecule
+	# charges = Partial charges from Mulliken population analysis (e)
 
-	natoms,nonHatoms,u0,atomlist,coords=[],[],[],[],[]
+	natoms,nonHatoms,Ea,charges,alpha,mu,atomlist,coords=[],[],[],[],[],[],[],[]
+
+	atomref=[-0.500273,-37.846772,-54.583861,-75.064579,-99.718730]		# Energies (Ha) of single atoms [H,C,N,O,F]
+	atoms=['H','C','N','O','F']
 
 	# Loop over all selected indices (molecules)
 
 	for i in ind:
 
-		xyz,elemtype,nnonH=[],[],0      # Initialize list that will contain coordinates and element types of ith molecule
+		xyz,elemtype,mulliken,nnonH=[],[],[],0      # Initialize list that will contain coordinates and element types of ith molecule
 		i = str(i).zfill(6)     	# This pads the index with zeros so that all contain 6 digits (e.g. index 41 -> 000041)
 		
 		# Define the path to the .xyz file of ith molecule. Here it is assumed that the dataset is stored in a 
@@ -47,21 +53,28 @@ def preprocess(datasize):
 					na = int(line)      # Number of atoms in molecule
 					natoms.append(na)
 				elif j == 1:
-					u0.append(float(line.split()[12]))  # Properties written on second line. 12th index is u0
+					E = float(line.split()[12])  				# Properties written on second line. Atomization energy,
+					mu.append(float(line.split()[5])) 			# Dipole moment,
+					alpha.append(float(line.split()[6])) 		# Polarizability
 				elif 2 <= j <= na+1:
-					parts = line.split()                # Lines 2 -> na+1 contains element types, coordinates and charges
-					elemtype.append(parts[0])           # Index 0 = element type, 1 = x, 2 = y, 3 = z
+					parts = line.split()                	# Lines 2 -> na+1 contains element types, coordinates and charges
+					elemtype.append(parts[0])           	# Index 0 = element type, 1 = x, 2 = y, 3 = z
+					mulliken.append(parts[4])				# Partial charge on atom
+					E = E - atomref[atoms.index(parts[0])]	# Subtract energy of isolated atom from total energy
 					if parts[0] != 'H':
 						nnonH += 1
 					xyz.append(np.array([float(parts[1]),float(parts[2]),float(parts[3])]))
 
+		Ea.append(E*27.21139)			# Convert atomization energy from Ha to eV
 		atomlist.append(elemtype)
 		coords.append(xyz)
 		nonHatoms.append(nnonH)
+		charges.append(mulliken)
 
 	# Return all lists in the form of numpy arrays
 
-	return np.array(natoms),np.array(u0),np.array(atomlist),np.array(coords),np.array(nonHatoms)
+	return np.array(natoms),np.array(Ea),np.array(mu),np.array(charges),np.array(alpha), \
+		np.array(atomlist),np.array(coords),np.array(nonHatoms)
 
 
 ## Implement the MBTR descriptor here! Azeema?
@@ -95,7 +108,7 @@ def coulomb(natoms,atomlist,coords):
 				if j == k:
 					M[i][j][k] = 0.5*Z[atoms.index(atomlist[i][j])]**2.4	# atoms.index(atomlist[i][j]) identifies the 
 																			# atomtype and its index in the 'atoms' list and
-																			# then gets the correct nuclear charge from 'Z'
+																			# then gets the corresponding nuclear charge from 'Z'
 				else:
 					M[i][j][k] = Z[atoms.index(atomlist[i][j])]*Z[atoms.index(atomlist[i][k])]/np.linalg.norm(coords[i][j]-coords[i][k])
 		
@@ -108,12 +121,16 @@ def coulomb(natoms,atomlist,coords):
 
 	return Mvec
 
-# Do the grid search (if optimal hyperparameters are not known), then training and prediction using KRR
+## Do the grid search (if optimal hyperparameters are not known), then training and prediction using KRR
+## If doing grid search for optimal parameters use small training set size, like 1k (takes forever otherwise)
 
 def train(x,y,nonHatoms):
 
-	x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25,stratify=nonHatoms)
-	kr = KernelRidge(kernel='laplacian',alpha=1e-12,gamma=1e-4) 				# 1e-11 and 1e-4 are optimal for CM + u0
+	# Split the selected data into a training set (90%) and a test set (10%), stratified using the number of non-H atoms
+	# To do: (1) add learning curves, (2) optimize hyperparameters for mu and alpha, (3) figure out how to handle the partial charges
+
+	x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1,stratify=nonHatoms)
+	kr = KernelRidge(kernel='laplacian',alpha=1e-11,gamma=1e-4) 		# 1e-11 and 1e-4 are optimal for CM, Ea, laplacian
 	
 	#kr = GridSearchCV(KernelRidge(kernel='laplacian'), cv=5,param_grid={"alpha": np.logspace(-12,0,13),"gamma": np.logspace(-6,0,7)})
 	#print(kr.best_params_)		# Print optimal hyperparameters
@@ -121,23 +138,48 @@ def train(x,y,nonHatoms):
 	kr.fit(x_train,y_train)
 	y_pred = kr.predict(x_test)
 
-	print(MAE(y_test,y_pred),MSE(y_test,y_pred))
+	print(MAE(y_test,y_pred),np.sqrt(MSE(y_test,y_pred)))		# Print mean absolute error and root mean squared error
 
 	return y_pred,y_test
 
-# The main routine
+
+## The main routine and plotting
 
 def main():
 
-	datasize=20000
-	natoms,u0,atomlist,coords,nonHatoms = preprocess(datasize)
+	datasize=10000
+	natoms,Ea,mu,charges,alpha,atomlist,coords,nonHatoms = preprocess(datasize)
 	Mvec = coulomb(natoms,atomlist,coords)
 
-	y_pred,y_test=train(Mvec,u0,nonHatoms)
+	Ea_pred,Ea_test=train(Mvec,Ea,nonHatoms)
+	mu_pred,mu_test=train(Mvec,mu,nonHatoms)
+	alpha_pred,alpha_test=train(Mvec,alpha,nonHatoms)
 
+	# Just some plot settings
+	plt.rc('text', usetex=True)
+	plt.rc('font', family='serif', size=14)
+
+	#Plot results for Ea
 	plt.figure()
-	plt.plot(y_test,y_pred,'b.')
-	plt.plot(np.linspace(y_test.min(),y_test.max(),1000),np.linspace(y_test.min(),y_test.max(),1000),'k--')
+	plt.plot(Ea_test,Ea_pred,'b.')
+	plt.plot(np.linspace(Ea_test.min(),Ea_test.max(),1000),np.linspace(Ea_test.min(),Ea_test.max(),1000),'k--')
+	plt.xlabel(r'$\Delta_\mathrm{at}E^\mathrm{DFT}$ (eV)')
+	plt.ylabel(r'$\Delta_\mathrm{at}E^\mathrm{KRR}$ (eV)')
+
+	#Plot results for mu
+	plt.figure()
+	plt.plot(mu_test,mu_pred,'b.')
+	plt.plot(np.linspace(mu_test.min(),mu_test.max(),1000),np.linspace(mu_test.min(),mu_test.max(),1000),'k--')
+	plt.xlabel(r'$\mu^\mathrm{DFT}$ (D)')
+	plt.ylabel(r'$\mu^\mathrm{KRR}$ (D)')
+
+	#Plot results for alpha
+	plt.figure()
+	plt.plot(alpha_test,alpha_pred,'b.')
+	plt.plot(np.linspace(alpha_test.min(),alpha_test.max(),1000),np.linspace(alpha_test.min(),alpha_test.max(),1000),'k--')
+	plt.xlabel(r'$\alpha^\mathrm{DFT}$ (e)')
+	plt.ylabel(r'$\alpha^\mathrm{KRR}$ (e)')
+
 	plt.show()
 
 if __name__ == '__main__':
